a2071e1055c480e0e15ac6de439d492daa23a7e4
Bing Pei <bpei@vmware.com>
2016-08-24 21:06:20 +0800

    selenium

---
+++ easytesting/src/main/java/com/easytesting/seleniumhelper/WebDriverFactory.java
@@ -0,0 +1,93 @@
+package com.easytesting.seleniumhelper;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebDriverFactory {
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(WebDriverFactory.class);
+
+
+	public static WebDriver getDefaultChromeDriver() {
+		System.setProperty("webdriver.chrome.driver","selenium-driver//chromedriver.exe");
+		log.info("Getting chrome driver...");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	
+	public static WebDriver getDefaultFirefoxDriver() {
+		log.info("Getting firefox driver...");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+
+	public static WebDriver getDefaultIEDriver() {
+		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+		System.setProperty("webdriver.ie.driver","selenium-driver//IEDriverServer.exe");	
+		
+		log.info("Getting IE driver...");
+		WebDriver driver = new InternetExplorerDriver(caps);
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static WebDriver getChromeDriverWithURL(String url) {
+		System.setProperty("webdriver.chrome.driver","selenium-driver//chromedriver.exe");
+		log.info("Getting chrome driver...");
+		WebDriver driver= new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	
+	public static WebDriver getDefaultFirefoxDriverWithURL(String url) {
+		log.info("Getting firefox driver...");
+		WebDriver driver = new FirefoxDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+
+	public static WebDriver getDefaultIEDriverWithURL(String url) {
+		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+		System.setProperty("webdriver.ie.driver","selenium-driver//IEDriverServer.exe");	
+		
+		log.info("Getting IE driver...");
+		WebDriver driver = new InternetExplorerDriver(caps);
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static WebDriver getSpecificFirefoxDriver(String installPath) {
+		System.setProperty("webdriver.firefox.bin", installPath); 
+		log.info("Getting firefox driver...");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static WebDriver getSpecificFirefoxDriverWithURL(String url, String installPath) {
+		System.setProperty("webdriver.firefox.bin", installPath); 
+		log.info("Getting firefox driver...");
+		WebDriver driver = new FirefoxDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+}
