634c32fc413d7d89dd6d016d86112a5dec9c8462
Bing Pei <1bingshui@sina.com>
2016-09-21 18:14:05 +0800

    soap client based on cxf

--- easytesting/src/main/java/com/easytesting/ws/EasySoapClient.java
+++ easytesting/src/main/java/com/easytesting/ws/EasySoapClient.java
@@ -1,5 +1,66 @@
 package com.easytesting.ws;
 
-public class EasySoapClient {
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.log4j.Logger;
+
+
+public class EasySoapClient {
+	public static final Logger logger = Logger.getLogger(EasySoapClient.class);
+	
+	private static EasySoapClient easySoapClient;
+	
+	
+	private EasySoapClient() {}
+	
+	private static JaxWsProxyFactoryBean jaxWsProxyFactoryBean;
+	
+	public static EasySoapClient newInstance(Class<?> serviceClass, String endpoint) {
+		jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+		jaxWsProxyFactoryBean.setServiceClass(serviceClass);
+		jaxWsProxyFactoryBean.setAddress(endpoint);
+		easySoapClient = new EasySoapClient();
+		return easySoapClient;
+	}
+	
+	public static <T> T getWebService() {
+		T serviceClient = (T) jaxWsProxyFactoryBean.create();
+		return serviceClient;
+	}
+	
+	public static <T> T getWebServiceByHttps() {
+		T serviceClient = (T) jaxWsProxyFactoryBean.create();
+		TLSClientParameters tlsParams = new TLSClientParameters();
+		TrustManager[] tm = null;
+		tm[0] = new MyTrustManager();
+		tlsParams.setTrustManagers(tm);
+		tlsParams.setDisableCNCheck(true);
+		Client proxy = ClientProxy.getClient(serviceClient);
+		((HTTPConduit) proxy.getConduit()).setTlsClientParameters(tlsParams);
+		return serviceClient;
+	}
+	
+    private static class MyTrustManager implements  X509TrustManager{
+		
+		@Override
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+			return new java.security.cert.X509Certificate[]{};
+		}
+		
+		@Override
+		public void checkServerTrusted(java.security.cert.X509Certificate[] chain,
+				String authType) throws java.security.cert.CertificateException {
+		}
+		
+		@Override
+		public void checkClientTrusted(java.security.cert.X509Certificate[] chain,
+				String authType) throws java.security.cert.CertificateException {
+		}
+	};
 }
